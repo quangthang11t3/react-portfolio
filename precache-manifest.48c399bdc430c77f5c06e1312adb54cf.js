30bb3762f16d256f7dbe9cbf5ccc6bf012eb5832
quangthang11t3 <quangthang11t3@gmail.com>
2021-07-24 21:00:23 +0900

    Updates

---
+++ precache-manifest.48c399bdc430c77f5c06e1312adb54cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499524d84d7df4d286b78072bdee7e12",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ca48e15fb57646c06103",
+    "url": "/react-portfolio/static/css/2.dfb7c7bb.chunk.css"
+  },
+  {
+    "revision": "0cb9db6b2a42bb38f0ac",
+    "url": "/react-portfolio/static/css/main.6d856035.chunk.css"
+  },
+  {
+    "revision": "ca48e15fb57646c06103",
+    "url": "/react-portfolio/static/js/2.16f653d9.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/react-portfolio/static/js/2.16f653d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb9db6b2a42bb38f0ac",
+    "url": "/react-portfolio/static/js/main.238f333c.chunk.js"
+  },
+  {
+    "revision": "6f6086f91a7ae7353c00",
+    "url": "/react-portfolio/static/js/runtime-main.873154d3.js"
+  }
+]);
